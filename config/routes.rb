2de0834a5febad00eba8b09a5bcfe28b6a866f33
Takekuni <takevadogu@outlook.jp>
2022-08-07 00:06:43 +0900

    hotwire-railsの導入と、Model/View/Controllerの作成を行った。

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # resources :rooms
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :rooms do
+    resources :messages
+  end
+end
